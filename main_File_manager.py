f67c7051a7ee45d8c368283a27b33e40e078036e
ilafedoseev <idfedoseev@groscloud.com>
2024-07-13 12:07:44 +1000

    цикличный ввод & замечания

--- main_File_manager.py
+++ main_File_manager.py
@@ -1,25 +1,32 @@
 import os
 import os.path
 
+# TODO: Добавить примечания для пользователя
+# TODO: Сделать просмотр содержимого папок
+
 
 path = '.'
 
-rez = sorted(os.listdir(path))
-n: int
-for n, item in enumerate(rez):
-    print(n+1, item)
+while True:
+    rez = sorted(os.listdir(path))
+    n: int
+    for n, item in enumerate(rez):
+        print(n+1, item)
+    target_index = 0
 
-n_item =int(input("Введите номер элемента: "))
-
-if n_item <= len(rez) and n_item > 0:
-    item = rez[n_item - 1]
-    if os.path.isfile(item):
-        with open(item, 'r') as file:
-            content = file.read()
-            print(content)
-elif n_item == 0:
-    print("введеный номер не должен быть равен 0")
-except ValueError:
-    print("Введённый номер не является числом")
-else:
-    print("Введённый номер не соответствует количеству элементов списка")
+    try:
+        target_index = int(input("Введите номер элемента: "))
+    except:
+        print("Error")
+        continue
+    if target_index <= len(rez) and target_index > 0:
+        item = rez[target_index - 1]
+        if os.path.isfile(item):
+            with open(item, 'r') as file:
+                content = file.read()
+                print(content)
+    elif target_index == 0:
+        print("Завершение работы...")
+        break
+    else:
+        print("Введённый номер не соответствует количеству элементов списка")
